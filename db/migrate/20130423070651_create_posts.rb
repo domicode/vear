24ea1527bbf798e91a4bf326c43cecadfe81ec9b
Andi Staub (CyT) <andi.staub@cyt.ch>
2013-04-23 09:18:10 +0200

    Update migration and run them

--- db/migrate/20130423070651_create_posts.rb
+++ db/migrate/20130423070651_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.integer :kind
-      t.string :message
+      t.integer :kind, :null => false
+      t.string :message, :limit => 140, :null => false
 
       t.timestamps
     end
