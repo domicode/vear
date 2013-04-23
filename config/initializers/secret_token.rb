bd3a6a10b5955f3f8e57791f57132a462517b5e0
Andi Staub (CyT) <andi.staub@cyt.ch>
2013-04-23 08:50:24 +0200

    Initialize git repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vear::Application.config.secret_token = '8ef08450ce1f2ac84ed171759c346fa589b40a72a8c180b5473be68010e778d37ba9820dd3805e1c570d9448a068887971429f0f3bb4b7ad7bf429e201f8eaf0'
