a6890f8e42babd4cf1a2885ad17b2a83523e31ee
Andi Staub (CyT) <andi.staub@cyt.ch>
2013-04-23 09:35:57 +0200

    Add validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :kind, :message
+
+  validates :kind, :presence => true, :numericality => { :only_integer => true, :less_than_or_equal_to => 1 }
+  validates :message, :presence => true, :length => { :in => 10..140 }
 end
