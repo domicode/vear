0cd417e595094502b55fb8b612ed802f5c2ab5ca
Andi Staub (CyT) <andi.staub@cyt.ch>
2013-04-23 09:56:29 +0200

    Add create and index action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,14 @@
 class PostsController < InheritedResources::Base
+  def index
+    @post = Post.new
+
+    index! { root_url }
+  end
+
+  def create
+    create! do |success, failure|
+      success.html { redirect_to root_url}
+      failure.html { render action: 'index' }
+    end
+  end
 end
