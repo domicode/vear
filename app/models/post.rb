14b9748af5e05d3a31a1e49030e5d3f2f3929c3d
Andi Staub (CyT) <andi.staub@cyt.ch>
2013-04-26 13:21:21 +0200

    Finalize text search

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,18 +7,38 @@ class Post < ActiveRecord::Base
   validates :kind, :presence => true, :numericality => { :only_integer => true, :less_than_or_equal_to => 1 }
   validates :message, :presence => true, :length => { :in => 10..140 }
 
-  def to_indexed_json
-    to_json :except => ['updated_at']
+  settings :analysis => {
+    :filter => {
+      :substring  => {
+        "type"     => "nGram",
+        "min_gram" => 1,
+        "max_gram" => 20
+      }
+    },
+    :analyzer => {
+      :str_search_analyzer => {
+        "tokenizer"    => "keyword",
+        "filter"       => "lowercase"
+      },
+      :str_index_analyzer => {
+        "tokenizer"    => "keyword",
+        "filter"       => ["lowercase", "substring"]
+      }
+    }
+  } do
+    mapping { indexes :message, :type => 'string', :index_analyzer => 'str_index_analyzer', :search_analyzer => 'str_search_analyzer' }
   end
 
   def self.search(params)
     params[:kind] = 1 if params[:kind] == 0
     params[:kind] = 0 if params[:kind] == 1
 
-    tire.search do
-      query { string params[:query].squish } if params[:query].present?
+    tire.search(:load => true) do
+      query { text(:message, params[:query].squish) } if params[:query].present?
+      highlight :message
+      sort { by :_score, :desc }
       filter :term, :kind => params[:kind] if params[:kind].present?
-      sort { by :created_at, 'desc' }
+      size 100
     end
   end
 end
