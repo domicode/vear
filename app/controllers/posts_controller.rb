c036b3188a90c54c41982999eeb40306c92bcc28
Andi Staub (CyT) <andi.staub@cyt.ch>
2013-04-29 09:02:21 +0200

    Fix redirect bug on search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < InheritedResources::Base
   respond_to :js
+
   def index
     @post = Post.new
 
-    @posts = Post.page(params[:page])
+    @posts = Post.search(params)
 
-    index! { root_url }
   end
 
   def create
@@ -13,11 +13,5 @@ class PostsController < InheritedResources::Base
       success.html { redirect_to root_url}
       failure.html { render action: 'index' }
     end
-  end
-
-  def search
-    @post = Post.new
-
-    @posts = Post.search(params)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Vear::Application.routes.draw do
   resources :posts do
     collection do
       get 'page/:page', :action => :index
-      get :search
     end
   end
 
