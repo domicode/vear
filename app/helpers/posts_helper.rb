0e9f2f41eff01cb3a95bb3f98ceda0f81f449c07
Andi Staub (CyT) <andi.staub@cyt.ch>
2013-05-02 19:00:36 +0200

    Remove unuesed helpermethod in post helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,13 +1,2 @@
 module PostsHelper
-  def color_kind(resource)
-    if resource.kind == 1
-      content_tag(:div, '', :id => "#{resource.id}", :class => 'post offer') do
-        content_tag(:p, resource.message)
-      end
-    else
-      content_tag(:div, '', :id => "#{resource.id}", :class => 'post demand') do
-        content_tag(:p, resource.message)
-      end
-    end
-  end
 end
