e86b1cec5def171339896191e10b7d9b01a229e6
Andi Staub (CyT) <andi.staub@cyt.ch>
2013-05-03 10:05:06 +0200

    Drop nested routes, because they are useless

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,6 @@
 class PostsController < InheritedResources::Base
-  before_filter :load_current_user
-
   def index
-    @post = @user.posts.build if @user.present?
+    @post = Post.new
     @posts = Post.search(params)
-  end
-
-  def create
-    @post = @user.posts.create(params[:post])
-    create! { user_posts_path(@user) }
-  end
-
-  def destroy
-    @post = @user.posts.find(params[:id])
-    destroy!
-  end
-
-  def load_current_user
-    @user = current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Vear::Application.routes.draw do
-  devise_for :users, :controllers => {sessions: 'sessions'}
+  devise_for :users
 
   resources :users
+
+  resources :posts
 
   authenticated :user do
     root :to => 'users#index'
   end
-
-  resources :users do
-    resources :posts
-  end
-
-  resources :posts
 
   root :to => 'posts#index'
 end
